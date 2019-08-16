83696c6f6f5fb7c1b556adca9007068a70ca8bd0
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-08-16 21:04:30 +0530

    build error solved

--- src/app/help/help-request-list/help-request-list.component.ts
+++ src/app/help/help-request-list/help-request-list.component.ts
@@ -3,11 +3,10 @@ import { HelpQuery } from 'src/app/shared/models/help-query.model';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { map, tap } from 'rxjs/operators';
-import { GetInterviewsByUserId } from 'src/app/core/store/actions/interview.actions';
 import { User } from 'src/app/shared/models/user.model';
 import { AppState } from 'src/app/core/store/state/app.state';
 import { Store } from '@ngrx/store';
-import { selectAllHelpRequestsList, selectQueries } from 'src/app/core/store/selectors/help.selectors';
+import { selectQueries } from 'src/app/core/store/selectors/help.selectors';
 import { GetHelpRequestsByUserId } from 'src/app/core/store/actions/help.actions';
 
 @Component({

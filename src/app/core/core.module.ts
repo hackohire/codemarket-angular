72fab202929d49690737c0bb1c9ee81ba5faa799
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-08-22 18:34:56 +0530

    typo error solved

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -109,7 +109,7 @@ export class CoreModule {
                   kind: 'UNION',
                   name: 'descriptionBlocks',
                   possibleTypes: [
-                    { name: 'HeaderhBlock' },
+                    { name: 'HeaderBlock' },
                     { name: 'ParagraphBlock' },
                     { name: 'CodeBlock' },
                     { name: 'ImageBlock' },

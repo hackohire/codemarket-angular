5aabb3bd3e61987bfe217f99fd931214d899e482
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-10 18:26:40 +0530

    While saving Bugfixes, show the respective sweetalert based on the status of the bugfixes

--- src/app/core/store/effects/product.effects.ts
+++ src/app/core/store/effects/product.effects.ts
@@ -5,7 +5,7 @@ import { AppState } from '../state/app.state';
 import { AddPrdouct, GetProductsByUserId, ProductList, UpdatePrdouct, ProductUpdated, GetProductById, SetSelectedProduct, GetAllProducts, SetAllProductsList, DeleteProduct } from '../actions/product.actions';
 import { map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
 import { SellingProductsService } from 'src/app/selling/selling-products.service';
-import { Product } from 'src/app/shared/models/product.model';
+import { Product, ProductStatus } from 'src/app/shared/models/product.model';
 import { SweetalertService } from 'src/app/shared/services/sweetalert.service';
 import { ProductService } from '../../services/product.service';
 import { selectProductsList } from '../selectors/product.selectors';
@@ -19,9 +19,9 @@ export class ProductEffects {
         private productService: ProductService,
         private sweetAlertService: SweetalertService,
         private store: Store<AppState>
-    ) {}
+    ) { }
 
-    @Effect({dispatch: false})
+    @Effect({ dispatch: false })
     addProduct$ = this.actions$.pipe(
         ofType(AddPrdouct),
         map(action => action.product),
@@ -29,81 +29,96 @@ export class ProductEffects {
         tap(u => console.log(u)),
         tap((product: Product) => {
             console.log(product);
-            this.sweetAlertService.success('Product Added Successfully', '', 'success');
+
+            if (product && product.status === ProductStatus.Drafted) {
+                this.sweetAlertService.success('Bugfix has been Drafted Successfully', '', 'success');
+            }
+
+            if (product && product.status === ProductStatus.Published) {
+                this.sweetAlertService.success('Bugfix has been Published Successfully', '', 'success');
+            }
+
             // return new ProductAdded(product);
         })
     );
 
-    @Effect()
-    updateProduct$ = this.actions$.pipe(
-        ofType(UpdatePrdouct),
-        map(action => action.product),
-        switchMap((product) => this.sellingProductService.updateProduct(product)),
-        tap(u => console.log(u)),
-        map((product: Product) => {
-            console.log(product);
-            this.sweetAlertService.success('Product Updated Successfully', '', 'success');
-            return ProductUpdated({product});
-        })
-    );
+@Effect()
+updateProduct$ = this.actions$.pipe(
+    ofType(UpdatePrdouct),
+    map(action => action.product),
+    switchMap((product) => this.sellingProductService.updateProduct(product)),
+    tap(u => console.log(u)),
+    map((product: Product) => {
+        console.log(product);
 
-    @Effect()
-    getProductsByUserId$ = this.actions$.pipe(
-        ofType(GetProductsByUserId),
-        map((value) => value.userId),
-        switchMap((userId: string) => this.sellingProductService.getProductsByUserId(userId)),
-        tap(u => console.log(u)),
-        map((product: Product[]) => {
-            console.log(product);
-            return ProductList({product});
-        })
-    );
+        if (product && product.status === ProductStatus.Published) {
+            this.sweetAlertService.success('Bugfix changes has been Published Successfully', '', 'success');
+        }
 
-    @Effect()
-    getProductById$ = this.actions$.pipe(
-        ofType(GetProductById),
-        map(action => action.productId),
-        switchMap((productId) => this.productService.getProductById(productId)),
-        tap(u => console.log(u)),
-        map((product) => {
-            console.log(product);
-            return SetSelectedProduct({product});
-        })
-    );
+        if (product && product.status === ProductStatus.Unpublished) {
+            this.sweetAlertService.success('Bugfix changes has been Unpublished Successfully', '', 'success');
+        }
+
+        return ProductUpdated({ product });
+    })
+);
+
+@Effect()
+getProductsByUserId$ = this.actions$.pipe(
+    ofType(GetProductsByUserId),
+    switchMap((value) => this.sellingProductService.getProductsByUserId(value.userId, value.status)),
+    tap(u => console.log(u)),
+    map((product: Product[]) => {
+        console.log(product);
+        return ProductList({ product });
+    })
+);
+
+@Effect()
+getProductById$ = this.actions$.pipe(
+    ofType(GetProductById),
+    map(action => action.productId),
+    switchMap((productId) => this.productService.getProductById(productId)),
+    tap(u => console.log(u)),
+    map((product) => {
+        console.log(product);
+        return SetSelectedProduct({ product });
+    })
+);
 
 
-    @Effect()
-    getAllProductsList$ = this.actions$.pipe(
-        ofType(GetAllProducts),
-        switchMap(() => this.productService.getAllProducts()),
-        tap(u => console.log(u)),
-        map((product) => {
-            console.log(product);
-            return SetAllProductsList({product});
-        })
-    );
+@Effect()
+getAllProductsList$ = this.actions$.pipe(
+    ofType(GetAllProducts),
+    switchMap(() => this.productService.getAllProducts()),
+    tap(u => console.log(u)),
+    map((product) => {
+        console.log(product);
+        return SetAllProductsList({ product });
+    })
+);
 
-    @Effect()
-    deleteProduct$ = this.actions$.pipe(
-        ofType(DeleteProduct),
-        map(action => action.productId),
-        switchMap((productId: string) => {
-            return this.productService.deleteProduct(productId).pipe(
-                withLatestFrom(this.store.select(selectProductsList)),
-                map(([isDeleted, products]) => {
-                    if (isDeleted && products && products.length) {
-                        let deletedProductIndex = -1;
-                        deletedProductIndex = products.findIndex((p) => p._id === productId);
-                        if (deletedProductIndex > -1) {
-                            products.splice(deletedProductIndex, 1);
-                        }
-                        return [...products];
+@Effect()
+deleteProduct$ = this.actions$.pipe(
+    ofType(DeleteProduct),
+    map(action => action.productId),
+    switchMap((productId: string) => {
+        return this.productService.deleteProduct(productId).pipe(
+            withLatestFrom(this.store.select(selectProductsList)),
+            map(([isDeleted, products]) => {
+                if (isDeleted && products && products.length) {
+                    let deletedProductIndex = -1;
+                    deletedProductIndex = products.findIndex((p) => p._id === productId);
+                    if (deletedProductIndex > -1) {
+                        products.splice(deletedProductIndex, 1);
                     }
-                })
-            );
-        }),
-        map((product: Product[]) => {
-            return ProductList({product});
-        }),
-    );
+                    return [...products];
+                }
+            })
+        );
+    }),
+    map((product: Product[]) => {
+        return ProductList({ product });
+    }),
+);
 }

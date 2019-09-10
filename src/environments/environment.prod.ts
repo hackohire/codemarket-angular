686cd0f51661c56b8919ae92fff891b16bba133d
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-10 14:18:01 +0530

    default redirects fro auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,10 +13,10 @@ export const environment = {
     scope: ['email', 'profile', 'openid'],
 
     // Callback URL
-    redirectSignIn: 'https://codemarket.io', // or 'exp://127.0.0.1:19000/--/', 'myapp://main/'
+    redirectSignIn: 'https://codemarket-fe.herokuapp.com', // or 'exp://127.0.0.1:19000/--/', 'myapp://main/'
 
     // Sign out URL
-    redirectSignOut: 'https://codemarket.io', // or 'exp://127.0.0.1:19000/--/', 'myapp://main/'
+    redirectSignOut: 'https://codemarket-fe.herokuapp.com', // or 'exp://127.0.0.1:19000/--/', 'myapp://main/'
 
     // 'code' for Authorization code grant,
     // 'token' for Implicit grant

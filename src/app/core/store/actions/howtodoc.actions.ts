27548a9f310e894e1a4ab9c975dc704e8fb89191
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-11 17:54:18 +0530

    "How to doc" & "Testing" NGRX Actions created
    NGRX Actions are used to tell NGRX to perform particular task/action
    
    For Example: AddTesting is an action which will be dispatched with testing form data to add in the datatbase

---
+++ src/app/core/store/actions/howtodoc.actions.ts
@@ -0,0 +1,76 @@
+import { createAction, props } from '@ngrx/store';
+import { Howtodoc } from 'src/app/shared/models/howtodoc.model';
+
+export enum EHowtodocActions {
+    AddHowtodoc = '[Howtodoc] Add Howtodoc',
+    HowtodocAddedSuccessfully = '[Howtodoc] Howtodoc Added Successfully',
+
+    UpdateHowtodoc = '[Howtodoc] Update Howtodoc',
+    HowtodocUpdated = '[Howtodoc] Howtodoc Updated',
+    DeleteHowtodoc = '[Howtodoc] Delete Howtodoc',
+
+    GetAllHowtodocs = '[Howtodoc] Get All Howtodocs', // List of All Howtodocs in the platform
+    GetHowtodocsByUserId = '[Howtodoc] Get Howtodocs By User Id',
+    GetHowtodocById = '[Howtodoc] Get Howtodoc By Id',
+    HowtodocList = '[Howtodoc] HowtodocList', // List of Howtodocs Created by LoggedIn User
+
+    SetAllHowtodocsList = '[Howtodoc] Set All Howtodocs List', // Set the List of All Howtodocs within the platform
+    SetSelectedHowtodoc = '[Howtodoc] Select Howtodoc'
+
+
+}
+
+export const AddHowtodoc = createAction(
+    EHowtodocActions.AddHowtodoc,
+    props<{howtodoc: Howtodoc}>()
+);
+
+export const HowtodocAddedSuccessfully = createAction(
+    EHowtodocActions.HowtodocAddedSuccessfully,
+    props<{howtodoc: Howtodoc}>()
+);
+
+
+export const GetHowtodocsByUserId = createAction(
+    EHowtodocActions.GetHowtodocsByUserId
+);
+
+export const GetHowtodocById = createAction(
+    EHowtodocActions.GetHowtodocById,
+    props<{howtodocId: string}>()
+);
+
+export const HowtodocList = createAction(
+    EHowtodocActions.HowtodocList,
+    props<{howtodoc: Howtodoc[]}>()
+);
+
+export const SetSelectedHowtodoc = createAction(
+    EHowtodocActions.SetSelectedHowtodoc,
+    props<{howtodoc: Howtodoc}>()
+);
+
+export const GetAllHowtodocs = createAction(
+    EHowtodocActions.GetAllHowtodocs
+);
+
+export const SetAllHowtodocsList = createAction(
+    EHowtodocActions.SetAllHowtodocsList,
+    props<{howtodoc: Howtodoc[]}>()
+);
+
+export const UpdateHowtodoc = createAction(
+    EHowtodocActions.UpdateHowtodoc,
+    props<{howtodoc: Howtodoc}>()
+);
+
+export const HowtodocUpdated = createAction(
+    EHowtodocActions.HowtodocUpdated,
+    props<{howtodoc: Howtodoc}>()
+);
+
+export const DeleteHowtodoc = createAction(
+    EHowtodocActions.DeleteHowtodoc,
+    props<{howtodocId: string}>()
+);
+
---
+++ src/app/core/store/actions/testing.actions.ts
@@ -0,0 +1,76 @@
+import { createAction, props } from '@ngrx/store';
+import { Testing } from 'src/app/shared/models/testing.model';
+
+export enum ETestingActions {
+    AddTesting = '[Testing] Add Testing',
+    TestingAddedSuccessfully = '[Testing] Testing Added Successfully',
+
+    UpdateTesting = '[Testing] Update Testing',
+    TestingUpdated = '[Testing] Testing Updated',
+    DeleteTesting = '[Testing] Delete Testing',
+
+    GetAllTestings = '[Testing] Get All Testings', // List of All Testings in the platform
+    GetTestingsByUserId = '[Testing] Get Testings By User Id',
+    GetTestingById = '[Testing] Get Testing By Id',
+    TestingList = '[Testing] TestingList', // List of Testings Created by LoggedIn User
+
+    SetAllTestingsList = '[Testing] Set All Testings List', // Set the List of All Testings within the platform
+    SetSelectedTesting = '[Testing] Select Testing'
+
+
+}
+
+export const AddTesting = createAction(
+    ETestingActions.AddTesting,
+    props<{testing: Testing}>()
+);
+
+export const TestingAddedSuccessfully = createAction(
+    ETestingActions.TestingAddedSuccessfully,
+    props<{testing: Testing}>()
+);
+
+
+export const GetTestingsByUserId = createAction(
+    ETestingActions.GetTestingsByUserId
+);
+
+export const GetTestingById = createAction(
+    ETestingActions.GetTestingById,
+    props<{testingId: string}>()
+);
+
+export const TestingList = createAction(
+    ETestingActions.TestingList,
+    props<{testing: Testing[]}>()
+);
+
+export const SetSelectedTesting = createAction(
+    ETestingActions.SetSelectedTesting,
+    props<{testing: Testing}>()
+);
+
+export const GetAllTestings = createAction(
+    ETestingActions.GetAllTestings
+);
+
+export const SetAllTestingsList = createAction(
+    ETestingActions.SetAllTestingsList,
+    props<{testing: Testing[]}>()
+);
+
+export const UpdateTesting = createAction(
+    ETestingActions.UpdateTesting,
+    props<{testing: Testing}>()
+);
+
+export const TestingUpdated = createAction(
+    ETestingActions.TestingUpdated,
+    props<{testing: Testing}>()
+);
+
+export const DeleteTesting = createAction(
+    ETestingActions.DeleteTesting,
+    props<{testingId: string}>()
+);
+

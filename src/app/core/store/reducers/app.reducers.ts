57e3f52dcae90394b9399faed13b6965d0f62945
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-23 06:47:45 +0530

    removed all the NGRX Files related to specific post types

--- src/app/core/store/reducers/app.reducers.ts
+++ src/app/core/store/reducers/app.reducers.ts
@@ -3,14 +3,7 @@ import { AppState } from '../state/app.state';
 import { routerReducer } from '@ngrx/router-store';
 import { userReducers } from './user.reducers';
 import { productReducers } from './product.reducers';
-import { helpReducers } from './help.reducers';
 import { cartReducers } from './cart.reducers';
-import { interviewReducers } from './interview.reducers';
-import { requirementReducers } from './requirement.reducers';
-import { testingReducers } from './testing.reducers';
-import { howtodocReducers } from './howtodoc.reducers';
-import { designReducers } from './design.reducers';
-import { goalReducers } from './goal.reducers';
 import { postReducers } from './post.reducers';
 
 export function appReducesrs(): ActionReducerMap<AppState, any> {
@@ -18,14 +11,7 @@ export function appReducesrs(): ActionReducerMap<AppState, any> {
         router: routerReducer,
         users: userReducers,
         products: productReducers,
-        queries: helpReducers,
         cart: cartReducers,
-        interview: interviewReducers,
-        requirement: requirementReducers,
-        testing: testingReducers,
-        howtodoc: howtodocReducers,
-        design: designReducers,
-        goal: goalReducers,
         post: postReducers
     };
 }

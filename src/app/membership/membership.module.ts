642866c1a9f3fff5f72dad651a0b15ecb444b6e2
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-24 17:58:10 +0530

    build error solved

--- src/app/membership/membership.module.ts
+++ src/app/membership/membership.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MembershipComponent } from './membership.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
-import { MembershipListComponent } from './membership-list/membership-list.component';
 
 const memborshipRoutes: Routes = [
   {
@@ -13,7 +12,7 @@ const memborshipRoutes: Routes = [
 ];
 
 @NgModule({
-  declarations: [MembershipComponent, MembershipListComponent],
+  declarations: [MembershipComponent],
   imports: [
     CommonModule,
     SharedModule,

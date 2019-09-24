03660cf66c32888a1e5c00aa5183544edb00054a
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-24 17:57:37 +0530

    build error solved

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../core/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { QuillModule } from 'ngx-quill';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { BreadcumbComponent } from './components/breadcumb/breadcumb.component';
 import { HighlightModule } from 'ngx-highlightjs';
@@ -36,7 +35,6 @@ export function hljsLanguages() {
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
-    QuillModule.forRoot(),
     SweetAlert2Module.forRoot({
       buttonsStyling: false,
       customClass: 'modal-content',
@@ -59,7 +57,6 @@ export function hljsLanguages() {
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
-    QuillModule,
     HighlightModule,
     FlexLayoutModule,
     SweetAlert2Module,

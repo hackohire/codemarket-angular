eea68f0531beebe8124a4c253334d8f0cae6edf3
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-27 17:54:19 +0530

    open and listen to changes to websocket if call get from other person

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
-import { Observable } from 'rxjs/internal/Observable';
 import { timer } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from './core/store/state/app.state';
 import { GetCartProductsList } from './core/store/actions/cart.actions';
-import { switchMap, map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+import { MatDialog } from '@angular/material';
+import { VideoChatComponent } from './video-chat/video-chat.component';
+import { WebsocketService } from './shared/services/websocket.service';
 
 @Component({
   selector: 'app-root',
@@ -16,10 +18,12 @@ export class AppComponent {
   title = 'codemarket';
   constructor(
     private authService: AuthService,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    public dialog: MatDialog,
+    private webSocketService: WebsocketService
   ) {
 
-    const source = timer(0, 300000);
+    const source = timer(0, 600000);
 
     const subscribe = source.subscribe(val => {
       this.authService.checkIfUserIsLoggedIn();
@@ -29,11 +33,31 @@ export class AppComponent {
     this.authService.loggedInUser$.pipe(
       tap((u) => {
         if (u) {
-          return this.store.dispatch(GetCartProductsList());
+          this.webSocketService.subject.subscribe(async (d: any) => {
+            console.log(d);
+            if (d && d.userWhoIsCalling && !this.dialog.openDialogs.length) {
+              this.openDialog(d.channel);
+            }
+          });
+
+          this.store.dispatch(GetCartProductsList());
         }
       })
     ).subscribe();
 
+  }
+
+  openDialog(channel): void {
+    const dialogRef = this.dialog.open(VideoChatComponent, {
+      width: '550px',
+      data: {isSomeoneCalling: true, channel},
+      disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.animal = result;
+    });
   }
 }
 

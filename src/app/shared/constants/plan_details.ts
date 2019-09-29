792b09823f8994dc45a851857d8f3a86823bae3e
appdatasoft <sumi@appdatasoft.com>
2019-09-29 11:28:48 -0700

    3 plans

--- src/app/shared/constants/plan_details.ts
+++ src/app/shared/constants/plan_details.ts
@@ -2,16 +2,16 @@ export const plans = [
     {
         id: 'P-6L545048NJ143234WLWER2CI',
         product_id: 'PROD-55E73002WF4721926',
-        name: 'Unlimited Plan',
+        name: 'Developer Plan',
         status: 'ACTIVE',
-        description: 'Unlimited plan',
+        description: 'Career Plan',
         billing_cycles: [
             {
                 pricing_scheme: {
                     version: 1,
                     fixed_price: {
                         currency_code: 'USD',
-                        value: '145.0'
+                        value: '39.99/development/month'
                     },
                     create_time: '2019-09-23T19:29:13Z',
                     update_time: '2019-09-23T19:29:13Z'
@@ -62,16 +62,76 @@ export const plans = [
     {
         id: 'P-6WS74521Y1808750PLWENKCQ',
         product_id: 'PROD-55E73002WF4721926',
-        name: 'Ambition Plan',
+        name: 'Team Plan',
         status: 'ACTIVE',
-        description: 'Ambition plan',
+        description: 'Team plan',
         billing_cycles: [
             {
                 pricing_scheme: {
                     version: 1,
                     fixed_price: {
                         currency_code: 'USD',
-                        value: '99.0'
+                        value: '35.99/user/month'
+                    },
+                    create_time: '2019-09-23T14:22:02Z',
+                    update_time: '2019-09-23T14:22:02Z'
+                },
+                frequency: {
+                    interval_unit: 'MONTH',
+                    interval_count: 1
+                },
+                tenure_type: 'REGULAR',
+                sequence: 1,
+                total_cycles: 6
+            }
+        ],
+        payment_preferences: {
+            auto_bill_outstanding: true,
+            setup_fee: {
+                currency_code: 'USD',
+                value: '0.0'
+            },
+            setup_fee_failure_action: 'CONTINUE',
+            payment_failure_threshold: 3
+        },
+        taxes: {
+            percentage: '10.0',
+            inclusive: false
+        },
+        quantity_supported: false,
+        create_time: '2019-09-23T14:22:02Z',
+        update_time: '2019-09-23T14:22:02Z',
+        links: [
+            {
+                href: 'https://api.sandbox.paypal.com/v1/billing/plans/P-6WS74521Y1808750PLWENKCQ',
+                rel: 'self',
+                method: 'GET'
+            },
+            {
+                href: 'https://api.sandbox.paypal.com/v1/billing/plans/P-6WS74521Y1808750PLWENKCQ',
+                rel: 'edit',
+                method: 'PATCH'
+            },
+            {
+                href: 'https://api.sandbox.paypal.com/v1/billing/plans/P-6WS74521Y1808750PLWENKCQ/deactivate',
+                rel: 'self',
+                method: 'POST'
+            }
+        ]
+    },
+    {
+        id: 'P-6WS74521Y1808750PLWENKCQ',
+        product_id: 'PROD-55E73002WF4721926',
+        name: 'Coporate Plan',
+        status: 'ACTIVE',
+        description: 'Company plan',
+        billing_cycles: [
+            {
+                pricing_scheme: {
+                    version: 1,
+                    fixed_price: {
+                        currency_code: 'USD',
+                        value: '29.99/user/month'
                     },
                     create_time: '2019-09-23T14:22:02Z',
                     update_time: '2019-09-23T14:22:02Z'

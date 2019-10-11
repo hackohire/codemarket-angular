cee662918ba600442a4418069831241ef59636ba
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-10-11 15:33:43 +0530

    my-rsvp events datatable separatfrom common datatable component

--- src/app/event/my-rsvp/my-rsvp.component.ts
+++ src/app/event/my-rsvp/my-rsvp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BreadCumb } from '../../shared/models/bredcumb.model';
 import { Event } from '../../shared/models/event.model';
 import { PostService } from '../../shared/services/post.service';
@@ -7,6 +7,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../core/store/state/app.state';
 import { selectLoggedInUser } from '../../core/store/selectors/user.selector';
 import { map, tap, switchMap } from 'rxjs/operators';
+import { MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-my-rsvp',
@@ -17,6 +18,9 @@ export class MyRsvpComponent implements OnInit {
 
   breadcumb: BreadCumb;
   myRSVPEvents: Event[] = [];
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  dataSource = new MatTableDataSource();
+  displayedColumns = ['number', 'name', 'from', 'to', 'host', 'action'];
 
   constructor(
     public postService: PostService,
@@ -42,15 +46,34 @@ export class MyRsvpComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.dataSource.sort = this.sort;
     this.store.select(selectLoggedInUser).pipe(
       switchMap((u) => {
-        if(u) {
+        if (u) {
           return this.postService.myRSVP(u._id)
         }
       }),
-      tap((u) => this.myRSVPEvents = u)
+      tap((u) => {
+        console.log(u);
+        /** Set the data for the datatable  */
+        this.dataSource.data = [...u];
+
+        /** Set the columns visible in the table */
+        // this.displayedColumns = ['number', 'name', 'price', 'createdBy', 'type', 'category', 'createdAt'];
+      })
     ).subscribe();
+  }
+
+  cancelRSVP(eventId: string) {
+    this.postService.cancelRSVP(eventId).subscribe((e) => {
+      console.log(e);
+      if (e && e.usersAttending && e.usersAttending) {
+        const isCustomerGoing = e.usersAttending.find(u => u._id === this.authService.loggedInUser._id);
+        if (!isCustomerGoing) {
+          this.dataSource.data = this.dataSource.data.filter((e: Event) => e._id !== eventId);
+        }
+      }
+    })
   }
 
 }

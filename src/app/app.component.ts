3eafe1ec12bdef4bdfcecc9752300b14d138f5e2
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-01-02 20:25:27 +0530

    commented peerjs related code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { GetCartProductsList } from './core/store/actions/cart.actions';
 import { tap } from 'rxjs/operators';
 import { MatDialog } from '@angular/material';
 import { VideoChatComponent } from './video-chat/video-chat.component';
-import { UserService } from './user/user.service';
-import Peer from 'peerjs';
+// import { UserService } from './user/user.service';
+// import Peer from 'peerjs';
 import { environment } from '../environments/environment';
 import { isPlatformBrowser, DOCUMENT } from '@angular/common';
 import { RouteHelperService } from './core/services/route-helper.service';
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit, OnDestroy {
     private authService: AuthService,
     private store: Store<AppState>,
     public dialog: MatDialog,
-    private userService: UserService,
+    // private userService: UserService,
     private routeHelper: RouteHelperService,
     private seoService: SeoService
   ) {
@@ -51,19 +51,19 @@ export class AppComponent implements OnInit, OnDestroy {
         tap((u) => {
           if (u) {
 
-            const peer = new Peer(u._id);
+            // const peer = new Peer(u._id);
 
-            peer.on('open', (id) => {
-              console.log('My peer ID is: ' + id);
-              if (id) {
-                this.userService.peer.next(peer);
-              }
-            });
+            // peer.on('open', (id) => {
+            //   console.log('My peer ID is: ' + id);
+            //   if (id) {
+            //     this.userService.peer.next(peer);
+            //   }
+            // });
 
-            peer.on('call', (call) => {
-              console.log(call);
-              this.openDialog(call, peer);
-            });
+            // peer.on('call', (call) => {
+            //   console.log(call);
+            //   this.openDialog(call, peer);
+            // });
             this.store.dispatch(GetCartProductsList());
           }
         })

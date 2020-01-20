817d4c7fea30e0730197f1c3b5fc263579bc46e4
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-01-21 02:01:27 +0530

    add company code cleanup

--- src/app/companies/add-company/add-company.component.ts
+++ src/app/companies/add-company/add-company.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Inject } from '@angular/core';
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
 import { BreadCumb } from '../../shared/models/bredcumb.model';
 import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { MatAutocomplete, MatChipInputEvent } from '@angular/material';
+import { MatAutocomplete } from '@angular/material';
+// import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 import { AuthService } from '../../core/services/auth.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { FormService } from '../../shared/services/form.service';
-import { startWith, map, catchError } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { Subscription, of } from 'rxjs';
 import { Tag } from '../../shared/models/product.model';
 import { CompanyService } from '../company.service';
@@ -23,7 +24,6 @@ import { LocationService } from '../../shared/services/location.service';
 export class AddCompanyComponent implements OnInit {
 
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
-  urlRegex = '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$';
   breadcumb: BreadCumb;
   companyForm: FormGroup;
   modules = {
@@ -55,7 +55,6 @@ export class AddCompanyComponent implements OnInit {
     return this.companyForm.get('questions');
   }
 
-
   get citiesFormControl() {
     return this.companyForm.get('cities') as FormArray;
   }
@@ -64,10 +63,6 @@ export class AddCompanyComponent implements OnInit {
     return this.companyForm.get('location');
   }
 
-  // get statusFormControl() {
-  //   return this.companyForm.get('status');
-  // }
-
   visible = true;
   selectable = true;
   removable = true;
@@ -75,8 +70,6 @@ export class AddCompanyComponent implements OnInit {
 
   subscription$: Subscription;
 
-  searchText = new FormControl();
-  citySuggestions: Tag[];
   allCities: Tag[];
 
   /** Location Variables */
@@ -92,9 +85,12 @@ export class AddCompanyComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private formService: FormService,
     private companyService: CompanyService,
-    public locationService: LocationService
+    public locationService: LocationService,
+    // @Inject(MAT_DIALOG_DATA) public data: any,
+    // public dialogRef: MatDialogRef<AddCompanyComponent>
   ) {
-    const queryParams: any = this.activatedRoute.snapshot.queryParams;
+
+    // console.log('dialogdata', this.data);
     const params: any = this.activatedRoute.snapshot.params;
 
     this.breadcumb = {
@@ -117,17 +113,14 @@ export class AddCompanyComponent implements OnInit {
      * get the company by fetching id from the params
      */
 
-    this.companyFormInitialization(null);
-    if (this.activatedRoute.snapshot.routeConfig.path === 'add-company') {
-      // this.companyFormInitialization(null);
+    // this.companyFormInitialization(null);
+    if (params.companyId) {
+      this.subscription$ = this.companyService.getCompanyById(params.companyId).subscribe((c) => {
+        this.edit = true;
+        this.companyFormInitialization(c);
+      });
     } else {
-      if (params.companyId) {
-        this.subscription$ = this.companyService.getCompanyById(params.companyId).subscribe((c) => {
-          this.edit = true;
-          this.companyFormInitialization(c);
-        });
-      }
-
+      this.companyFormInitialization(null);
     }
 
     // this.companyFormInitialization();
@@ -140,7 +133,7 @@ export class AddCompanyComponent implements OnInit {
     this.companyForm = await new FormGroup({
       name: new FormControl(i && i.name ? i.name : '', Validators.required),
       type: new FormControl(i && i.type ? i.type : ''),
-      cities: this.fb.array(i && i.cities && i.cities.length ? i.cities : []),
+      cities: new FormControl(i && i.cities && i.cities.length ? i.cities : []),
       description: new FormControl(i && i.description ? i.description : ''),
       // ideas: new FormControl(i && i.ideas ? i.ideas : ''),
       // questions: new FormControl(i && i.questions ? i.questions : ''),
@@ -154,23 +147,16 @@ export class AddCompanyComponent implements OnInit {
       }),
     });
 
+    // if (this.data) {
+    //   this.companyForm.controls['name'].setValue(this.data.name);
+    // }
+
     await this.locationService.setLocaionSearhAutoComplete(this.searchLocation, this.locationFormGroup);
 
     this.formService.findFromCollection('', 'cities').subscribe((cities) => {
-      this.citySuggestions = cities;
       this.allCities = cities;
     });
 
-    this.searchText.valueChanges.pipe(
-      startWith(''),
-      map((text) => text ? this._filter(text) : this.allCities && this.allCities.length ? this.allCities.slice() : []))
-      .subscribe((cities) => this.citySuggestions = cities);
-
-  }
-
-  private _filter(value): Tag[] {
-    const filterValue = value && value.name ? value.name.toLowerCase() : value.toLowerCase();
-    return this.allCities.filter(city => city.name.toLowerCase().indexOf(filterValue) === 0);
   }
 
   submit(status) {
@@ -195,7 +181,11 @@ export class AddCompanyComponent implements OnInit {
         })
       ).subscribe((d: any) => {
         Swal.fire(`${d.name} has been Created Successfully`, '', 'success').then(() => {
-          this.companyService.redirectToCompanyDetails(d._id);
+          // if (this.data) {
+            // this.dialogRef.close(d);
+          // } else {
+            this.companyService.redirectToCompanyDetails(d._id);
+          // }
         });
         this.companyFormInitialization(d);
       });
@@ -207,49 +197,19 @@ export class AddCompanyComponent implements OnInit {
           return of(false);
         })
       )
-      .subscribe((d: any) => {
-        if (d) {
-          Swal.fire(`${d.name} has been Updated Successfully`, '', 'success').then(() => {
-            this.companyService.redirectToCompanyDetails(d._id);
-          });
-          this.companyFormInitialization(d);
-        }
-      });
-    }
-  }
-  
-  addTech(event: MatChipInputEvent): void {
-    if (!this.matAutocomplete.isOpen) {
-      const availableTag = this.citySuggestions.find((t) => t.name.toLowerCase() == event.value.trim().toLowerCase());
-      const formAvailableInTafsFormControl = this.citiesFormControl.value.find((t) => t.name.toLowerCase() == event.value.trim().toLowerCase());
-      if (formAvailableInTafsFormControl) {
-        event.input.value = '';
-      } else if (availableTag) {
-        this.citiesFormControl.push(new FormControl({ name: availableTag.name, _id: availableTag._id }));
-      } else {
-        this.formService.addCategory(this.citiesFormControl, event);
-      }
-      // this.tagSuggestions = this.tagSuggestions.filter((t) => t.name.toLowerCase() !== event.value.trim().toLowerCase())
-      this.searchText.setValue(null);
+        .subscribe((d: any) => {
+          if (d) {
+            Swal.fire(`${d.name} has been Updated Successfully`, '', 'success').then(() => {
+              this.companyService.redirectToCompanyDetails(d._id);
+            });
+            this.companyFormInitialization(d);
+          }
+        });
     }
   }
 
-  selected(event) {
-    // this.tagSuggestions = this.tagSuggestions.filter((t) => t._id !== event.option.value._id)
-    const formAvailableInTafsFormControl = this.citiesFormControl.value.find((t) => t.name.toLowerCase() == event.option.value.name.trim().toLowerCase());
-    if (formAvailableInTafsFormControl) {
-      event.input.value = '';
-    } else {
-      this.formService.selectedCategory(this.citiesFormControl, event);
-    }
-    this.searchInput.nativeElement.value = null;
-    this.searchText.setValue(null);
-  }
-
-
-  // Remove a Tag
-  public remove(index: number): void {
-    this.formService.removeCategory(this.citiesFormControl, index);
+  addCitiesFn(name) {
+    return { name };
   }
 }
 

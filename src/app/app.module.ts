02f44eebcd4b9f88499d98f8b49a5449a9189e4b
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-01-31 11:46:06 +0530

    removed all read more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import 'zone.js/dist/zone-patch-rxjs';
-import { HackohireComponent } from './read-more/hackohire/hackohire.component';
-import { WellnessComponent } from './read-more/wellness/wellness.component';
-import { SocialImpactComponent } from './read-more/social-impact/social-impact.component';
-import { TeamIqComponent } from './read-more/team-iq/team-iq.component';
-import { DreamJobComponent } from './read-more/dream-job/dream-job.component';
-import { StartupsComponent } from './read-more/startups/startups.component';
-import { LocalBusinessComponent } from './read-more/local-business/local-business.component';
-import { CodingExpertsComponent } from './read-more/coding-experts/coding-experts.component';
-import { GovernmentComponent } from './read-more/government/government.component';
-import { DiversityComponent } from './read-more/diversity/diversity.component';
 import { AmplifyModule } from './core/amplify/amplify.module';
 import { PostDataResolver } from './core/resolver';
 
@@ -26,17 +16,6 @@ import { PostDataResolver } from './core/resolver';
   declarations: [
     AppComponent,
     NavBarComponent,
-    HackohireComponent,
-    WellnessComponent,
-    SocialImpactComponent,
-    TeamIqComponent,
-    DreamJobComponent,
-    TeamIqComponent,
-    StartupsComponent,
-    LocalBusinessComponent,
-    CodingExpertsComponent,
-    GovernmentComponent,
-    DiversityComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),

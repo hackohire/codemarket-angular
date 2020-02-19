56601903250413cd30b4b75738e28500df1b2d91
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-02-19 18:05:53 +0530

    Email Service Created to call sendEmail graphql lambda function

---
+++ src/app/email/email.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Email } from '../shared/models/email.model';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { description } from '../shared/constants/fragments_constatnts';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  emailQuery = gql`
+    fragment Email on Email {
+      _id
+      createdAt
+      updatedAt
+      type
+      status
+
+      createdBy {
+        name
+        _id
+        avatar
+      }
+      to
+      cc
+      bcc
+      subject
+      description {
+        ...Description
+      }
+      descriptionHTML
+    }
+    ${description}
+  `;
+
+  constructor(
+    private apollo: Apollo,
+  ) { }
+
+  sendEmail(email: Email) {
+    return this.apollo.mutate(
+      {
+        mutation: gql`
+          mutation sendEmail($email: EmailInput) {
+            sendEmail(email: $email) {
+              ...Email
+            }
+          }
+          ${this.emailQuery}
+        `,
+        variables: {
+          email
+        },
+        fetchPolicy: 'no-cache'
+      }
+    ).pipe(
+      map((p: any) => {
+        return p.data.sendEmail;
+      }),
+    );
+  }
+}

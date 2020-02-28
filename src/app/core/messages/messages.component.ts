d822cf0ca147d2b627bc369e9f1d7192e7bffb1c
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-02-28 21:25:49 +0530

    Show messages based on the dates

--- src/app/core/messages/messages.component.ts
+++ src/app/core/messages/messages.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostService } from '../../shared/services/post.service';
 import { CompanyService } from '../../companies/company.service';
 import { MessageService } from '../../shared/services/message.service';
 import moment from 'moment';
 import { environment } from '../../../environments/environment';
 import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
+import { trigger, transition, query, stagger, animate, style, state } from '@angular/animations';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(1000)),
+    ]),
+  ]
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit, OnDestroy {
 
   messagesPageNumber = 1;
 
-  
   anonymousAvatar = '../../../../assets/images/anonymous-avatar.jpg';
   s3FilesBucketURL = environment.s3FilesBucketURL;
+  subscription$ = new Subscription();
+  uniqueDatesArray = [];
 
   constructor(
     public postService: PostService,
@@ -27,6 +38,21 @@ export class MessagesComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
+    /** Finding the different dates and putting it into the array to show messages based on the comments */
+    this.subscription$.add(
+      this.messageService.messagesList$.subscribe(mList => {
+        if (mList.messages && mList.messages.length) {
+          console.log(new Date(+(mList.messages[0].createdAt)).toISOString());
+          this.uniqueDatesArray = [...new Set(mList.messages.map(item => new Date(+(item.createdAt)).toISOString().split('T')[0]))];
+        }
+        console.log(this.uniqueDatesArray);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription$.unsubscribe();
   }
 
   redirectToPost(m) {
@@ -36,6 +62,10 @@ export class MessagesComponent implements OnInit {
     } else if (m.companyPost) {
       this.companyService.redirectToCompanyDetails(m.companyPost._id);
     }
+  }
+
+  convertDate(d) {
+    return new Date(+d).toISOString().split('T')[0];
   }
 
   fromNow(date) {

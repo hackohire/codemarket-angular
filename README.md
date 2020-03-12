f9eee1e854228100d283cd338b60704ef590a122
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-03-12 23:44:16 +0530

    Revert "frontend API for jane got created"
    This reverts commit cbabe34009ddbd7cec35c0fc45a57450f8be0fd9.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jane
+# medighee
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.3.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to jane!');
+    expect(page.getTitleText()).toEqual('Welcome to medighee!');
   });
 
   afterEach(async () => {

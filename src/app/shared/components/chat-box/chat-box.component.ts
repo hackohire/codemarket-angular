ad301ecb7dd10f8720c20dd37a333d603935d952
Jay <jaysojitra13@gmail.com>
2020-03-31 23:34:47 +0530

    [feature] chat between author and user

--- src/app/shared/components/chat-box/chat-box.component.ts
+++ src/app/shared/components/chat-box/chat-box.component.ts
@@ -1,55 +1,21 @@
 import { Component, OnInit, Input, AfterViewInit, EventEmitter, Output } from '@angular/core';
-import { CommentService } from '../../services/comment.service';
 import { FormGroup, FormControl } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { Post } from '../../models/post.model';
 
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
-  styleUrls: ['./chat-box.component.scss'],
-  providers: [CommentService]
+  styleUrls: ['./chat-box.component.scss']
 })
-export class ChatBoxComponent implements OnInit, AfterViewInit {
+export class ChatBoxComponent implements OnInit {
  
   @Input() comments: Comment[];
   @Input() loggedInUserId;
 
   @Input() postDetails;
 
-  commentForm: FormGroup;
-  type: string; // product | help-request | interview | requirement | Testing | Howtodoc
-  commentId: string;
-  blockId: string;
-  selectedBlock = null;
-
   constructor(
-    private activatedRoute: ActivatedRoute,
-    public commentService: CommentService,
   ) { }
 
   ngOnInit() {
-    console.log("THis is commentData in ngOnInit ==> ", this.commentService.commentsList$);
   }
-
-  ngAfterViewInit() {
-    console.log("THis is postDetails in ngAfterViewInit ==> ", this.postDetails);
-  }
-
-  initializeCommentForm(p, commentType?: string) {
-    this.commentForm = new FormGroup({
-      text: new FormControl(''),
-      referenceId: new FormControl(p._id),
-      type: new FormControl(commentType ? commentType : this.type),
-    });
-
-    this.commentService.getCommentsByReferenceId(p, this.commentId);
-
-    if (this.blockId) {
-      this.selectedBlock = this.postDetails.description.find((b: any) => b._id === this.blockId);
-      console.log(this.selectedBlock);
-    }
-
-  }
-
 }

65d19f3201f452f66775ef9b21adc9803402f62a
Vanrajsinh <mayurshin.vaghela43@gmail.com>
2020-04-03 20:17:59 +0530

    Added contact service filr.

---
+++ src/app/contact/contact.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import gql from 'graphql-tag';
+import { Apollo } from 'apollo-angular';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor(
+    private apollo: Apollo,
+  ) { }
+
+  getQuote(formFieldObject) {
+    return this.apollo.mutate(
+      {
+        /** Mutation Query */
+        mutation: gql`
+          mutation getContact($contact: ContactInput) {
+            getContact(contact: $contact) {
+              name
+              email
+              subject
+              description
+            }
+          }
+        `,
+        variables: {
+          contact: formFieldObject
+        },
+      }
+    ).pipe(
+      map((p: any) => {
+        return p.data.getContact;
+      }),
+    );
+
+}

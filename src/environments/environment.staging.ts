f1784706146c11395e99eeaf6e096e114a64cef4
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-04-10 12:58:15 +0530

    staging websocket api url changes

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -8,7 +8,7 @@ export const environment = {
   s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://mp6gs0sdx5.execute-api.us-east-1.amazonaws.com/staging',
+  webSocketURL: 'wss://rykjcx8k5f.execute-api.us-east-1.amazonaws.com/staging',
   // planDetails: plans.prod,
   planDetails: plans.prod,
   oauth: {

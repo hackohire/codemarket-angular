f15d3e3723844076dd0f1b426f0d216662adbd16
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-04-16 13:13:50 +0530

    Revert "new deployment"
    This reverts commit 9ad69fe9990f14be99684bc50bd0a5e0f23ea4b2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://hgo07730wk.execute-api.us-east-1.amazonaws.com/prod',
+  webSocketURL: 'wss://rn9gdz7ty7.execute-api.us-east-1.amazonaws.com/prod',
   // planDetails: plans.prod,
   planDetails: plans.prod,
   oauth: {

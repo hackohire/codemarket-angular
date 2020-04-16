ee686326e9614dc628ea454d78d342b4a8d0c19c
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-04-16 11:53:39 +0530

    codemarket backend API pointing changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ import { plans } from '../app/shared/constants/plan_details';
 export const environment = {
   production: true,
   COGNITO_AUTH_DOMAIN: 'https://platform.auth.us-east-1.amazoncognito.com',
-  serverless_url: 'https://codemarketapi-prod.codemarket.io/',
-  graphql_url: 'https://codemarketapi-prod.codemarket.io/graphql',
+  serverless_url: 'https://codemarket-api-prod.codemarket.io/',
+  graphql_url: 'https://codemarket-api-prod.codemarket.io/graphql',
   s3FilesBucketURL: 'https://codearket-upload-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',

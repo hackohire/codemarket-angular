b36cfa92b46dcd0743d046072cb6c3248552744f
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-04-16 11:05:18 +0530

    cognito client updated

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,12 +2,12 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:b08baf8a-8de9-45bc-a2d8-d537e23d2426",
+    // "aws_cognito_identity_pool_id": "us-east-1:b08baf8a-8de9-45bc-a2d8-d537e23d2426",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_B4xlOmGS0",
-    "aws_user_pools_web_client_id": "6b96cs2pmq0khunn6mth7r48td",
+    "aws_user_pools_id": "us-east-1_8rqnJmLTg",
+    "aws_user_pools_web_client_id": "4c2kbhcj41lgpgugogisp1ma2g",
     "oauth": {},
-    "aws_user_files_s3_bucket": "platformfile-dev",
+    "aws_user_files_s3_bucket": "codemarket-files",
     "aws_user_files_s3_bucket_region": "us-east-1"
 };
 

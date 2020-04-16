3384780f2b3106224fd22fa7df9a26ef98f44da1
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-04-16 11:28:40 +0530

    local websocket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   graphql_url: 'http://localhost:3200/graphql',
   s3FilesBucketURL: 'https://codearket-upload-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://vgvyg9uqe2.execute-api.us-east-1.amazonaws.com/prod',
+  webSocketURL: 'wss://localhost:3001',
   planDetails: plans.dev,
   oauth: {
     // Domain name

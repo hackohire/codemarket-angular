cbb0a56e1f8b071ed8f798b978b3074a95ceae5f
jaysojitra13 <jaysojitra13@gmail.com>
2020-04-25 18:37:38 +0530

    yoga env setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ import { plans } from '../app/shared/constants/plan_details';
 export const environment = {
   production: true,
   COGNITO_AUTH_DOMAIN: 'https://platform.auth.us-east-1.amazoncognito.com',
-  serverless_url: 'https://codemarketapi-prod.codemarket.io/',
-  graphql_url: 'https://codemarketapi-prod.codemarket.io/graphql',
-  s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
+  serverless_url: 'https://yogapi.codemarket.io/',
+  graphql_url: 'https://yogapi.codemarket.io/graphql',
+  s3FilesBucketURL: 'https://yogafiles.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://314t92s90a.execute-api.us-east-1.amazonaws.com/prod',
+  webSocketURL: 'wss://gi1beuczn0.execute-api.us-east-1.amazonaws.com/yoga',
   // planDetails: plans.prod,
   planDetails: plans.prod,
   oauth: {

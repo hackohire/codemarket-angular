f9148b74f68b518d8f2931097dd9c1d4eff758e6
jaysojitra13 <jaysojitra13@gmail.com>
2020-05-05 10:49:53 +0530

    saas env setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ import { plans } from '../app/shared/constants/plan_details';
 export const environment = {
   production: true,
   tiny_api: '5fsqmw7kewxh0ljo1cfr46zhxre6demtcb67n0nzg736c1s4',
-  serverless_url: 'https://codemarketapi-prod.codemarket.io/',
-  graphql_url: 'https://codemarketapi-prod.codemarket.io/graphql',
-  s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
+  serverless_url: 'https://saasapi.codemarket.io/',
+  graphql_url: 'https://saasapi.codemarket.io/graphql',
+  s3FilesBucketURL: 'https://saasfiles.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://314t92s90a.execute-api.us-east-1.amazonaws.com/prod',
+  webSocketURL: 'wss://bo6h6u8s7k.execute-api.us-east-1.amazonaws.com/saas',
   // planDetails: plans.prod,
   planDetails: plans.prod,
   oauth: {

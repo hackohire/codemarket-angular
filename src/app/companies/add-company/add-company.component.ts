4b000a799f5fd4ddefb8d9acaeb5f4edaed38c49
jaysojitra13 <jaysojitra13@gmail.com>
2020-05-09 18:19:02 +0530

    Remove description from company form

--- src/app/companies/add-company/add-company.component.ts
+++ src/app/companies/add-company/add-company.component.ts
@@ -114,7 +114,7 @@ export class AddCompanyComponent implements OnInit {
       name: new FormControl(i && i.name ? i.name : '', Validators.required),
       type: new FormControl(i && i.type ? i.type : ''),
       cities: new FormControl(i && i.cities && i.cities.length ? i.cities : []),
-      description: new FormControl(i && i.description ? i.description : ''),
+      // description: new FormControl(i && i.description ? i.description : ''),
       // ideas: new FormControl(i && i.ideas ? i.ideas : ''),
       // questions: new FormControl(i && i.questions ? i.questions : ''),
       createdBy: new FormControl(i && i.createdBy && i.createdBy._id ? i.createdBy._id : ''),

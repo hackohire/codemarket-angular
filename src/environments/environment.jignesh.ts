885a9d2c1ef536be175deb09532873b4eb3f2adf
jaysojitra13 <jaysojitra13@gmail.com>
2020-05-11 14:54:25 +0530

    jignesh env setup

--- src/environments/environment.jignesh.ts
+++ src/environments/environment.jignesh.ts
@@ -3,15 +3,15 @@ import { plans } from '../app/shared/constants/plan_details';
 export const environment = {
   production: true,
   tiny_api: '5fsqmw7kewxh0ljo1cfr46zhxre6demtcb67n0nzg736c1s4',
-  serverless_url: 'https://jaydipapi.codemarket.io/',
-  graphql_url: 'https://jaydipapi.codemarket.io/graphql',
-  s3FilesBucketURL: 'https://jaydip-files.s3.amazonaws.com/public/',
+  serverless_url: 'https://jigneshapi.codemarket.io/',
+  graphql_url: 'https://jigneshapi.codemarket.io/graphql',
+  s3FilesBucketURL: 'https://jignesh-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://snjtj94bwl.execute-api.us-east-1.amazonaws.com/jaydip',
+  webSocketURL: 'wss://muqky3soue.execute-api.us-east-1.amazonaws.com/jignesh',
   // planDetails: plans.prod,
   planDetails: plans.prod,
-  fileS3Bucket: 'jaydip-files',
+  fileS3Bucket: 'jignesh-files',
   oauth: {
     // Domain name
     domain: 'platform.auth.us-east-1.amazoncognito.com',

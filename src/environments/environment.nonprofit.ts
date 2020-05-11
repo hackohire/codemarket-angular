8444c913b76988569bdfa28a54383d99795b5870
jaysojitra13 <jaysojitra13@gmail.com>
2020-05-11 13:11:39 +0530

    nonprofit env setup

--- src/environments/environment.nonprofit.ts
+++ src/environments/environment.nonprofit.ts
@@ -3,15 +3,15 @@ import { plans } from '../app/shared/constants/plan_details';
 export const environment = {
   production: true,
   tiny_api: '5fsqmw7kewxh0ljo1cfr46zhxre6demtcb67n0nzg736c1s4',
-  serverless_url: 'https://la2050api.codemarket.io/',
-  graphql_url: 'https://la2050api.codemarket.io/graphql',
-  s3FilesBucketURL: 'https://la2050-files.s3.amazonaws.com/public/',
+  serverless_url: 'https://nonprofitapi.codemarket.io/',
+  graphql_url: 'https://nonprofitapi.codemarket.io/graphql',
+  s3FilesBucketURL: 'https://nonprofit-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: ' wss://2q88m6qblh.execute-api.us-east-1.amazonaws.com/la2050',
+  webSocketURL: 'wss://wg5znmqvj4.execute-api.us-east-1.amazonaws.com/nonprofit',
   // planDetails: plans.prod,
   planDetails: plans.prod,
-  fileS3Bucket: 'la2050-files',
+  fileS3Bucket: 'nonprofit-files',
   oauth: {
     // Domain name
     domain: 'platform.auth.us-east-1.amazoncognito.com',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,11 +10,11 @@ export const environment = {
   tiny_api: '5fsqmw7kewxh0ljo1cfr46zhxre6demtcb67n0nzg736c1s4',
   serverless_url: 'http://localhost:3200/dev/',
   graphql_url: 'http://localhost:3200/dev/graphql',
-  s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
+  s3FilesBucketURL: 'https://common-local-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
   webSocketURL: 'ws://localhost:3001',
   planDetails: plans.dev,
-  fileS3Bucket: 'la2050-files',
+  fileS3Bucket: 'common-local-files',
   oauth: {
     // Domain name
     domain: 'platform.auth.us-east-1.amazoncognito.com',

357434cb1c734f583922ba72bccb345545624043
jaysojitra13 <jaysojitra13@gmail.com>
2020-05-11 14:41:36 +0530

    jaydip env setup

--- src/environments/environment.jaydip.ts
+++ src/environments/environment.jaydip.ts
@@ -3,15 +3,15 @@ import { plans } from '../app/shared/constants/plan_details';
 export const environment = {
   production: true,
   tiny_api: '5fsqmw7kewxh0ljo1cfr46zhxre6demtcb67n0nzg736c1s4',
-  serverless_url: 'https://nonprofitapi.codemarket.io/',
-  graphql_url: 'https://nonprofitapi.codemarket.io/graphql',
-  s3FilesBucketURL: 'https://nonprofit-files.s3.amazonaws.com/public/',
+  serverless_url: 'https://jaydipapi.codemarket.io/',
+  graphql_url: 'https://jaydipapi.codemarket.io/graphql',
+  s3FilesBucketURL: 'https://jaydip-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://wg5znmqvj4.execute-api.us-east-1.amazonaws.com/nonprofit',
+  webSocketURL: 'wss://snjtj94bwl.execute-api.us-east-1.amazonaws.com/jaydip',
   // planDetails: plans.prod,
   planDetails: plans.prod,
-  fileS3Bucket: 'nonprofit-files',
+  fileS3Bucket: 'jaydip-files',
   oauth: {
     // Domain name
     domain: 'platform.auth.us-east-1.amazoncognito.com',

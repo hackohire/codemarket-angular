805abf73314ca5fa0d0a5b2ba5864208fcef05e2
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-05-14 01:11:43 +0530

    Ckeditor Credentials Added

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,12 @@
 import { plans } from '../app/shared/constants/plan_details';
 
 export const environment = {
+  ckEditor: {
+    ckEditorEnvironMentId: 'ZK5UoNVFJG2R3Kh4SrIC',
+    ckEditorSecretKey: 'HP771KcTLlvStVbtvwAGz5d3LhLgDlY50uJqNQ6nkS18kVP43ZPB1zOSLseK',
+    ws: '71258.cke-cs.com/ws',
+    developmentTokenUrl: 'https://71258.cke-cs.com/token/dev/qhNbD6xUjkeVtrEc7aD2PcMhDkgmirUJHunqRWESilB0mTzZ0RHcJYk68P6D'
+  },
   production: true,
   serverless_url: 'https://codemarketapi-staging.codemarket.io/',
   graphql_url: 'https://codemarketapi-staging.codemarket.io/graphql',

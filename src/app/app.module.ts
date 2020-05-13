bde3967019f0ca15119be8548ff4b25829a5f75c
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-05-13 11:48:27 +0530

    removed unneeded components & routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,35 +11,12 @@ import { LayoutModule } from '@angular/cdk/layout';
 import 'zone.js/dist/zone-patch-rxjs';
 import { PostDataResolver } from './core/resolver';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { InnovateComponent } from './innovate/innovate.component';
-import { AffordableComponent } from './affordable/affordable.component';
-import { GetWorkDoneComponent } from './get-work-done/get-work-done.component';
-import { DedicatedTeamComponent } from './dedicated-team/dedicated-team.component';
 import { WebsiteComponent } from './website/website.component';
 import { EmailMarketingComponent } from './email-marketing/email-marketing.component';
 import { SocialMediaMarketingComponent } from './social-media-marketing/social-media-marketing.component';
 import { CrmComponent } from './crm/crm.component';
-import { ReferralNetworkComponent } from './referral-network/referral-network.component';
-import { SalesGrowthStrategyComponent } from './sales-growth-strategy/sales-growth-strategy.component';
-import { AiStrategyComponent } from './ai-strategy/ai-strategy.component';
-import { ExitStrategyComponent } from './exit-strategy/exit-strategy.component';
-import { TrainingFromExpertsComponent } from './training-from-experts/training-from-experts.component';
-import { RealProjectsComponent } from './real-projects/real-projects.component';
-import { MockInterviewsComponent } from './mock-interviews/mock-interviews.component';
-import { DreamJobMentorsComponent } from './dream-job-mentors/dream-job-mentors.component';
-import { ResumeGapAnalysisComponent } from './resume-gap-analysis/resume-gap-analysis.component';
-import { PeerNetworkingComponent } from './peer-networking/peer-networking.component';
-import { LeadershipTrainingComponent } from './leadership-training/leadership-training.component';
-import { StickyComponent } from './sticky/sticky.component';
-import { ViralComponent } from './viral/viral.component';
-import { CommunityImpactStoriesComponent } from './community-impact-stories/community-impact-stories.component';
-import { AdvocatesInfluencersComponent } from './advocates-influencers/advocates-influencers.component';
 import { LoaderComponent } from './auth/loader/loader.component';
 import { AuthModule } from './auth/auth.module';
-import { StressManagementComponent } from './stress-management/stress-management.component';
-import { NewsletterComponent } from './newsletter/newsletter.component';
-import {VolunteerComponent} from './volunteer/volunteer.component';
-import {DonateComponent} from './donate/donate.component';
 import {MakemoneyonlineModule} from './makemoneyonline/makemoneyonline.module';
 import { FormioModule } from 'angular-formio';
 import {FormBuilderModule} from './form-builder/form-builder.module';
@@ -56,33 +33,10 @@ import { SelecteDeselectComponent } from './shared/components/selecte-deselect/s
 
     NavBarComponent,
     AboutUsComponent,
-    InnovateComponent,
-    AffordableComponent,
-    GetWorkDoneComponent,
-    DedicatedTeamComponent,
     WebsiteComponent,
     EmailMarketingComponent,
     SocialMediaMarketingComponent,
     CrmComponent,
-    ReferralNetworkComponent,
-    SalesGrowthStrategyComponent,
-    AiStrategyComponent,
-    ExitStrategyComponent,
-    TrainingFromExpertsComponent,
-    RealProjectsComponent,
-    MockInterviewsComponent,
-    DreamJobMentorsComponent,
-    ResumeGapAnalysisComponent,
-    PeerNetworkingComponent,
-    LeadershipTrainingComponent,
-    StickyComponent,
-    ViralComponent,
-    StressManagementComponent,
-    CommunityImpactStoriesComponent,
-    AdvocatesInfluencersComponent,
-    NewsletterComponent,
-    VolunteerComponent,
-    DonateComponent,
     SelecteDeselectComponent
   ],
   imports: [

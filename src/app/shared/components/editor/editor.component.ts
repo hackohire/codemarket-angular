b8c597e04b3996ae916d1aff79bb22f8490bb304
Sumit Vekariya <sumitvekariya7@gmail.com>
2020-05-24 02:20:24 +0530

    removed easy image finally

--- src/app/shared/components/editor/editor.component.ts
+++ src/app/shared/components/editor/editor.component.ts
@@ -222,6 +222,7 @@ export class EditorComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   myCustomUploadAdapterPlugin(editor) {
+    console.log(editor.plugins);
     editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
       // Configure the URL to the upload script in your back-end here!
       return new CustomUploadAdapter(loader);

50c498c1ced0325aab50356479f5f899d4dc3526
jaysojitra13 <jaysojitra13@gmail.com>
2020-05-29 18:03:47 +0530

    new env changes

---
+++ src/environments/environment.joel.ts
@@ -0,0 +1,44 @@
+import { plans } from '../app/shared/constants/plan_details';
+
+export const environment = {
+  ckEditor: {
+    ckEditorEnvironMentId: 'qWCSnjZdhKZIm2BDeR6e',
+    ckEditorSecretKey: 'vOhUDQFlGWDTaOpVQmaPPm47RA9zFQ00tB6dQRdIa6Dqtxn7Uf8iGEc8oCMb',
+    ws: '71258.cke-cs.com/ws',
+    developmentTokenUrl: 'https://71258.cke-cs.com/token/dev/qhNbD6xUjkeVtrEc7aD2PcMhDkgmirUJHunqRWESilB0mTzZ0RHcJYk68P6D'
+  },
+  production: true,
+  serverless_url: 'https://joel-api.codemarket.io/',
+  graphql_url: 'https://joel-api.codemarket.io/graphql',
+  s3FilesBucketURL: 'https://joel-upload-files.s3.amazonaws.com/public/',
+  stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
+  // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
+  webSocketURL: 'wss://563w2xf1o3.execute-api.us-east-1.amazonaws.com/joel',
+  // planDetails: plans.prod,
+  planDetails: plans.prod,
+  fileS3Bucket: 'codemarket-files',
+  oauth: {
+    // Domain name
+    domain: 'platform.auth.us-east-1.amazoncognito.com',
+
+    // Authorized scopes
+    scope: ['email', 'profile', 'openid'],
+
+    // 'code' for Authorization code grant,
+    // 'token' for Implicit grant
+    // Note that REFRESH token will only be generated when the responseType is code
+    responseType: 'code',
+
+    // optional, for Cognito hosted ui specified options
+    options: {
+      // Indicates if the data collection is enabled to support Cognito advanced security features. By default, this flag is set to true.
+      AdvancedSecurityDataCollectionFlag: true
+    },
+  },
+  baseHref: '/',
+  confirm: {
+    email: '',
+    password: '',
+    name: ''
+  }
+};

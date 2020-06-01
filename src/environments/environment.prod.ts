437636f527bb31cd1627bb334479c2af9bbd005b
Manav Soni <manav.soni@solutionanalysts.com>
2020-06-01 08:43:52 +0530

    Update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,11 @@ export const environment = {
     developmentTokenUrl: 'https://71258.cke-cs.com/token/dev/qhNbD6xUjkeVtrEc7aD2PcMhDkgmirUJHunqRWESilB0mTzZ0RHcJYk68P6D'
   },
   production: true,
-  serverless_url: 'https://codemarketapi-prod.codemarket.io/',
-  graphql_url: 'https://codemarketapi-prod.codemarket.io/graphql',
+  // serverless_url: 'https://codemarketapi-prod.codemarket.io/',
+  // graphql_url: 'https://codemarketapi-prod.codemarket.io/graphql',
+  // s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
+  serverless_url: 'https://manavapi.codemarket.io/',
+  graphql_url: 'https://manavapi.codemarket.io/graphql',
   s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
   stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
   // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',

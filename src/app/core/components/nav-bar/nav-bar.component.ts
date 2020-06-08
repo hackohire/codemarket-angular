51971c6b810055d246245312aa367c55114a48ea
Dhawal Patel <iamdhawalpatel@gmail.com>
2020-06-08 10:46:05 +0530

    Logo Update + Therapy Image Added

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -18,7 +18,7 @@ import { appConstants } from '../../../shared/constants/app_constants';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss'],
+  styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit, OnDestroy {
 

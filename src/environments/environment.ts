8e24b693f67803dec8da81ade6e4683d075bde81
Dhawal Patel <iamdhawalpatel@gmail.com>
2020-06-16 16:14:37 +0530

    My Profile + TS file update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,15 +15,13 @@ export const environment = {
     developmentTokenUrl: 'https://71258.cke-cs.com/token/dev/qhNbD6xUjkeVtrEc7aD2PcMhDkgmirUJHunqRWESilB0mTzZ0RHcJYk68P6D'
   },
   COGNITO_AUTH_DOMAIN: 'https://platform.auth.us-east-1.amazoncognito.com',
-  serverless_url: 'https://codemarketapi-prod.codemarket.io/',
-  graphql_url: 'https://codemarketapi-prod.codemarket.io/graphql',
+  serverless_url: 'http://localhost:3200/dev/',
+  graphql_url: 'http://localhost:3200/dev/graphql',
   s3FilesBucketURL: 'https://codemarket-files.s3.amazonaws.com/public/',
-  stripe_public_key: 'pk_live_EYCoYtpLszr802sOl89WeuW1',
-  // stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
-  webSocketURL: 'wss://314t92s90a.execute-api.us-east-1.amazonaws.com/prod',
-  // planDetails: plans.prod,
-  planDetails: plans.prod,
-  fileS3Bucket: 'codemarket-files',
+  stripe_public_key: 'pk_test_ighmL2U9UqAx0O4b8dKkSvdq',
+  webSocketURL: 'ws://localhost:3001',
+  planDetails: plans.dev,
+  fileS3Bucket: 'common-local-files',
   oauth: {
     // Domain name
     domain: 'platform.auth.us-east-1.amazoncognito.com',
